758db3569701ed5fd245d2e15bbe52a4958655a1
nsh1l <nsh1l@outlook.com>
2024-09-01 14:52:25 +0900

    Optimized

--- leetspeak.py
+++ leetspeak.py
@@ -1,32 +1,10 @@
-# /* coding: utf-8 */
-
 import csv
 
-# 辞書ファイル読み込み
+def main():
+    w = input("変換したい英数字を入力してください: ").upper()
+    with open('dict.csv') as f:
+        ls = dict(filter(None, csv.reader(f)))
+    print(w.translate(str.maketrans(ls)))
 
-
-with open('Leetspeak_dict.csv') as f:
-    ls_dict = dict(filter(None, csv.reader(f)))
-
-# リートスピークに変換
-
-
-def cvrt_text(data):
-    list_data = list(data)
-    converted_data = []
-
-    for i in list_data:
-        converted_data += ls_dict[i]
-
-    output_text = ''.join(converted_data)
-
-    return output_text
-
-# 本体プログラム
-
-
-print("変換したい英数字を入力してください")
-raw_input = input().upper()
-output = cvrt_text(raw_input)
-
-print("\n変換前: {} \n変換後: {}".format(raw_input, output))
+if __name__ == '__main__':
+    main()
